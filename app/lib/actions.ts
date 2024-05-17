950c1c2e8abe9eb4fc9f83d8f5d6f8193c03bcae
Enayat Afridi <inayat.pcdc@gmail.com>
2024-05-17 11:04:30 +0500

    feat: ch14, client-side validation and aria labeling...

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,7 +9,9 @@ const FormSchema = z.object({
   customerId: z.string({
     invalid_type_error: 'Please select a customer',
   }),
-  amount: z.coerce.number().gt(0, { message: 'Amount must be greater than $0' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Amount must be greater than $0' }),
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status',
   }),
@@ -20,13 +22,13 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export type State = {
-    errors?: {
-        customerId?: string[];
-        amount?: string[];
-        status?: string[];
-    };
-    message?: string | null;
-}
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
@@ -62,13 +64,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
   clearandredirect();
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing fields. Failed to update invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
